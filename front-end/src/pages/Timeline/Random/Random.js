28e8db0492d865696edaa0176d1f9dc4713d2a90
Uenderss <meajudabarto@gmail.com>
2022-09-20 12:41:08 -0300

    first commit

---
+++ front-end/src/pages/Timeline/Random/Random.js
@@ -0,0 +1,23 @@
+import React from "react";
+import useRecommendation from "../../../hooks/api/useRecommendation";
+import Recommendation from "../../../components/Recommendation";
+
+export default function Random() {
+  const { recommendation, updateRecommendation } = useRecommendation();
+
+  const handleUpdate = () => {
+    updateRecommendation(recommendation.id);
+  }
+
+  if (!recommendation) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Recommendation
+      {...recommendation}
+      onUpvote={handleUpdate}
+      onDownvote={handleUpdate}
+    />
+  );
+}
---
+++ front-end/src/pages/Timeline/Timeline.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import Header from "../../components/Header/Header";
+import Menu from "../../components/Menu/Menu";
+
+export default function Timeline() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Outlet />
+    </>
+  );
+}
