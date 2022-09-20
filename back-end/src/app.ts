d7207fe37423374bbeca9dff46c2372d9ae2edc9
Uenderss <meajudabarto@gmail.com>
2022-09-20 13:43:06 -0300

    correcoes no projeto

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,12 +1,12 @@
 import cors from "cors";
-import express from "express";
+import express,{json} from "express";
 import "express-async-errors";
-import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
-import recommendationRouter from "./routers/recommendationRouter.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
+import recommendationRouter from "./routers/recommendationRouter";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.use("/recommendations", recommendationRouter);
 app.use(errorHandlerMiddleware);
